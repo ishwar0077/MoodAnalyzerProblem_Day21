11b31c67fcb9f7139aa9a4ef597e6219e538925b
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-28 14:23:58 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood AnalyzerProgram");
+        }
+    }
+}
