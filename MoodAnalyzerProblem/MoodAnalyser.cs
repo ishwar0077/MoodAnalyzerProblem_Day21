c35f2eb683c7bca9da166c9c7c9fbb5e0e1317c9
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-29 18:44:30 +0530

    analyse Happy or Sad

---
+++ MoodAnalyzerProblem/MoodAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyser
+    {
+        public string Analyser(string message)
+        {
+            if (message.Contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Ability to Analyse Happy or Sat UC1
+            MoodAnalyser mood = new MoodAnalyser();
+            string str = mood.Analyser("I am in Sad Mood");
+            Console.WriteLine(str);
+        }
+    }
+}
