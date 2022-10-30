7fb02be6c9b45591b9092cc17e443e9b5d53f10f
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-10-30 19:19:01 +0530

    using coustructor Test case 1.2 should return Happy

--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             //Refactor Analyse UC1
-            MoodAnalyser mood = new MoodAnalyser("I am in Sad Mood");
+            MoodAnalyser mood = new MoodAnalyser("I am in Happy Mood");
             string str = mood.Analyser();
             Console.WriteLine(str);
         }
